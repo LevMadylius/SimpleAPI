0c56f74fea0511728b797b416aabf6f1fd432b1f
lmadylius <lmadylius@sourcescrub.com>
2023-05-29 16:16:22 +0300

    new endpoint random numbers

--- SimpleAPI/Controllers/TestController.cs
+++ SimpleAPI/Controllers/TestController.cs
@@ -7,7 +7,16 @@ namespace SimpleAPI.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
+        private readonly Random _random;
+        public TestController()
+        {
+            _random = new Random();
+        }
         [HttpGet]
         public Task<string> Get() => Task.FromResult("test");
+
+        [HttpGet]
+        [Route("random")]
+        public Task<int> Get(int number) => Task.FromResult(_random.Next(0, number));
     }
 }
